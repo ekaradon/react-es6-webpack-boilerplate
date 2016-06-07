01b51aa2a305fd404aa7e6382f0bafb20f1e7258
ekaradon <fayangora@gmail.com>
2016-06-07 10:20:09 +0200

    task: case where the props is swallowed by react-constant-elements

--- scripts/App.js
+++ scripts/App.js
@@ -1,10 +1,45 @@
 import React, {Component} from 'react';
 
-export default class App extends Component {
-  render() {
+const data = [1, 2, 3, 4];
+
+function Bar({number}) {
+    return <div>Number is {number}</div>
+}
+
+function Foo({item}) {
+
+    function give_number_through_a_fake_function() {
+        return item;
+    }
+
+    function render_bug() {
+        return (
+            <div>
+                <div>Here the bug</div>
+                <Bar number={item}/>
+            </div>
+        );
+    }
+
     return (
-      // Add your component markup and other subcomponent references here.
-      <h1>Hello, World!</h1>
+        <div>
+            The bug will occur
+            {render_bug()}
+        </div>
     );
-  }
+}
+
+function render_as_foo(item) {
+    return <Foo item={item}/>;
+}
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+                {data.map(render_as_foo)}
+            </div>
+        );
+    }
 }
